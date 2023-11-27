aef1da8450cea36c5dc1fab5eab89ebfc3c18484
drcpu <drcpu@protonmail.com>
2023-11-27 20:45:03 +0100

    [mempool] Update mempool page to use the new API endpoint

--- src/Pages/Mempool.js
+++ src/Pages/Mempool.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Card, Container, Spinner, Tab, Tabs } from "react-bootstrap";
 import { ResponsiveContainer, Bar, BarChart, CartesianGrid, XAxis, YAxis, Label, Tooltip } from "recharts";
 
+import ErrorCard from "../Components/ErrorCard";
+
 import DataService from "../Services/DataService";
 import TimeConverter from "../Services/TimeConverter";
 
@@ -28,38 +30,47 @@ export default class Mempool extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: true,
             error_value: "",
-            last_updated: "",
-            mempool_data_requests: [],
-            data_request_fees: [],
-            mempool_value_transfers: [],
-            value_transfer_fees: []
+            mempool_data_requests: null,
+            mempool_value_transfers: null,
         }
 
         this.getMempool = this.getMempool.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
 
     componentDidMount() {
-        this.getMempool();
+        this.getMempool("data_requests");
     }
 
     componentWillUnmount() {
         clearInterval(this.interval_id);
     }
 
-    getMempool() {
-        DataService.getMempool()
+    handleSelect(tab) {
+        if (tab === "data_requests" && this.state.mempool_data_requests === null) {
+            this.getMempool("data_requests");
+        }
+        else if (tab === "value_transfers" && this.state.mempool_value_transfers === null) {
+            this.getMempool("value_transfers");
+        }
+    }
+
+    getMempool(transaction_type) {
+        DataService.getMempool(transaction_type)
         .then(response => {
-            this.setState({
-                loading: false,
-                error_value: "",
-                last_updated: TimeConverter.convertUnixTimestamp(response.last_updated, "hour"),
-                mempool_data_requests: response.mempool_data_requests,
-                data_request_fees: response.data_request_fees,
-                mempool_value_transfers: response.mempool_value_transfers,
-                value_transfer_fees: response.value_transfer_fees,
-            });
+            if (transaction_type === "data_requests") {
+                this.setState({
+                    error_value: "",
+                    mempool_data_requests: response,
+                });
+            }
+            else if (transaction_type === "value_transfers") {
+                this.setState({
+                    error_value: "",
+                    mempool_value_transfers: response,
+                });
+            }
         })
         .catch(e => {
             console.log(e);
@@ -69,12 +80,39 @@ export default class Mempool extends Component {
         });
     }
 
-    generateMempoolChart(data, fees) {
+    generateMempoolChart(mempool) {
         var colors = ["#f94144", "#f3722c", "#8961e0", "#f9844a", "#f9c74f", "#90be6d", "#43aa8b", "#4d908e", "#577590", "#277da1"];
 
-        var bars = fees.map(function (fee, idx) {
+        // Get all unique fees
+        var unique_fees = new Set();
+        mempool.forEach(entry => {
+            entry.fee.forEach(fee => unique_fees.add(fee));
+        });
+
+        // Bucketize fees to get a smoother graph
+        var fees = [...unique_fees].sort();
+        var mapped_fees = {0: 0};
+        var bar_fees = new Set();
+        if (fees.length > 9) {
+            var boundary = fees.length / 9;
+            fees.forEach(function(fee, idx){
+                var boundary_fee = fees[Math.floor((Math.floor(idx / boundary) + 1) * boundary - 1)];
+                bar_fees.add(boundary_fee);
+                mapped_fees[fee] = boundary_fee;
+            });
+        }
+        else {
+            fees.forEach(function(fee) {
+                bar_fees.add(fee);
+                mapped_fees[fee] = fee;
+            });
+        }
+        bar_fees = [...bar_fees].sort();
+
+        // Build a bar for each bucketized fee
+        var bars = bar_fees.map(function (fee, idx) {
             return <Bar
-                dataKey={fee}
+                dataKey={mapped_fees[fee]}
                 stackId="1"
                 stroke={colors[idx % colors.length]}
                 fill={colors[idx % colors.length]}
@@ -82,11 +120,23 @@ export default class Mempool extends Component {
             />;
         });
 
+        // Create a mapping of fees to amounts (+ timestamp)
+        var timestamp_fee_amount = mempool.map(function(entry) {
+            var entry_to_dict = {"timestamp": entry.timestamp};
+            entry.fee.forEach(function(fee, idx) {
+                if (!(mapped_fees[fee] in entry_to_dict)) {
+                    entry_to_dict[mapped_fees[fee]] = 0;
+                }
+                entry_to_dict[mapped_fees[fee]] += entry.amount[idx];
+            });
+            return entry_to_dict;
+        });
+
         return (
             <Card style={{ "height": "75vh", "marginTop": "1rem", "border": "none" }}>
                 <Card.Body>
                     <ResponsiveContainer width="100%" height="100%">
-                        <BarChart data={data} margin={{ top: 10, right: 10, left: 10, bottom: 10 }}>
+                        <BarChart data={timestamp_fee_amount} margin={{ top: 10, right: 10, left: 10, bottom: 10 }}>
                             <CartesianGrid strokeDasharray="3 3" />
                             <XAxis dataKey="timestamp" tick={false} label="Time" />
                             <YAxis>
@@ -99,47 +149,45 @@ export default class Mempool extends Component {
                         </BarChart>
                     </ResponsiveContainer>
                 </Card.Body>
-                <Card.Text style={{ paddingLeft: "1.25rem", paddingRight: "1.25rem" }}>
-                    <small className="text-muted">Last updated: {this.state.last_updated}</small>
-                </Card.Text>
             </Card>
         );
     }
 
     render() {
-        const { loading, error_value } = this.state;
+        const { error_value, mempool_data_requests, mempool_value_transfers } = this.state;
 
-        let dataRequestPanel, valueTransferPanel;
         if (error_value === "") {
-            if (loading) {
-                dataRequestPanel = <Container fluid style={{ "paddingTop": "1rem" }}><Spinner animation="border" /></Container>;
-                valueTransferPanel = <Container fluid style={{ "paddingTop": "1rem" }}><Spinner animation="border" /></Container>;
-            }
-            else {
-                dataRequestPanel = this.generateMempoolChart(this.state.mempool_data_requests, this.state.data_request_fees);
-                valueTransferPanel = this.generateMempoolChart(this.state.mempool_value_transfers, this.state.value_transfer_fees);
-            }
+            return (
+                <Container fluid>
+                    <Card className="w-100 shadow p-1 mb-3 bg-white rounded" style={{ height: "85vh" }}>
+                        <Card.Body style={{ "paddingTop": "0.5rem" }}>
+                            <Tabs defaultActiveKey="data_requests" id="uncontrolled-tab-example" onSelect={this.handleSelect}>
+                                <Tab eventKey="data_requests" title="Data requests">
+                                    {
+                                        mempool_data_requests === null
+                                            ? <Spinner animation="border" />
+                                            : this.generateMempoolChart(mempool_data_requests)
+                                    }
+                                </Tab>
+                                <Tab eventKey="value_transfers" title="Value transfers">
+                                    {
+                                        mempool_value_transfers === null
+                                            ? <Spinner animation="border" />
+                                            : this.generateMempoolChart(mempool_value_transfers)
+                                    }
+                                </Tab>
+                            </Tabs>
+                        </Card.Body>
+                    </Card>
+                </Container>
+            );
         }
         else {
-            dataRequestPanel = <Container fluid style={{ "paddingTop": "1rem" }}>{error_value}</Container>;
-            dataRequestPanel = <Container fluid style={{ "paddingTop": "1rem" }}>{error_value}</Container>;
+            return (
+                <Container fluid style={{ "padding": "0px" }}>
+                    <ErrorCard errorValue={error_value} />;
+                </Container>
+            );
         }
-
-        return (
-            <Container fluid>
-                <Card className="w-100 shadow p-1 mb-3 bg-white rounded" style={{ height: "85vh" }}>
-                    <Card.Body style={{ "paddingTop": "0.5rem" }}>
-                        <Tabs defaultActiveKey="data_request" id="uncontrolled-tab-example">
-                            <Tab eventKey="data_request" title="Data requests">
-                                {dataRequestPanel}
-                            </Tab>
-                            <Tab eventKey="value_transfer" title="Value transfers">
-                                {valueTransferPanel}
-                            </Tab>
-                        </Tabs>
-                    </Card.Body>
-                </Card>
-            </Container>
-        );
     }
 }
