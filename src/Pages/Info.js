5c2800633b52bd45c30fbce1b1ff03db484da096
drcpu <drcpu@protonmail.com>
2025-03-11 20:30:12 +0100

    [info] Bump version to 2.0.0

--- src/Pages/Info.js
+++ src/Pages/Info.js
@@ -106,7 +106,7 @@ export default class Info extends Component {
                                         <FontAwesomeIcon icon={['fab', 'osi']} style={{marginRight: "0.25rem"}} size="sm"/>
                                     </span>
                                     <span>
-                                        {"Source code (version 1.5)"}
+                                        {"Source code (version 2.0.0)"}
                                     </span>
                                 </td>
                             </tr>
